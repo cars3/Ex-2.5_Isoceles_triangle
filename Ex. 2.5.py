f28223d0994bc535c2ecf9ec18569a732b008841
Amartya Sai Andra <49435859+cars3@users.noreply.github.com>
2021-09-15 20:33:43 +0530

    Add files via upload

---
+++ Ex. 2.5.py
@@ -0,0 +1,19 @@
+import numpy as np
+
+A = np.array((1, 5, -2))
+B = np.array((1, 6, 4))
+C = np.array((1, 7, -2))
+
+AB = np.linalg.norm(A - B)
+BC = np.linalg.norm(B - C)
+AC = np.linalg.norm(A - C)
+print(AB, BC, AC)
+
+if AB == AC:
+    print("With triangle ABC, this is an Isoceles Triangle")
+elif AB == BC:
+    print("With triangle ABC, this is an Isoceles Triangle")
+elif AC == BC:
+    print("With triangle ABC, this is an Isoceles Triangle")
+else:
+    print("With triangle ABC, this is not an Isoceles Triangle")
